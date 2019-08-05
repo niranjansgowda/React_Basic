56beb7078e76891bb838c315b501525b7fbadda7
niranjansgowda <niru.s.g@gmail.com>
2019-08-05 20:03:36 +0530

    first steps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Niru from './Niru/Niru.js'
 
 class App extends Component {
     render(){
         return (
             <div className="App">
               <h1>Test</h1>
+              <Niru name="Shreyas"/>
             </div>
           );
     }
---
+++ src/Niru/Niru.js
@@ -0,0 +1,13 @@
+import React, { Component } from  'react'
+
+class Niru extends Component {
+    render(){
+
+        return(
+            <h1>{this.props.name} Niru called</h1>
+        )
+    }
+}
+
+
+export default Niru
